22da2cd63707e7b04fb9ecd25add939f0c825f60
dianerdiana <dianerdiana.de@gmail.com>
2024-04-25 18:43:07 +0700

    Add filter feature

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -8,7 +8,7 @@ import { PromptType } from "@type/prompt";
 
 type PromptCardListProps = {
   data: PromptType[] | [];
-  handleTagClick: () => void;
+  handleTagClick: (tagName: string) => void;
 };
 
 const PromptCardList: React.FC<PromptCardListProps> = ({
@@ -33,40 +33,79 @@ const PromptCardList: React.FC<PromptCardListProps> = ({
 };
 
 const Feed = () => {
-  const [searchText, setSearchText] = useState("");
-  const [posts, setPosts] = useState<PromptType[] | []>([]);
+  const [allPosts, setAllPosts] = useState([]);
 
-  const handleSearchChange: ChangeEventHandler<HTMLInputElement> = (
-    e: Event
-  ) => {
-    e.preventDefault();
+  // Search states
+  const [searchText, setSearchText] = useState("");
+  const [searchTimeout, setSearchTimeout] = useState<NodeJS.Timeout | null>(null);
+  const [searchedResults, setSearchedResults] = useState([]);
+
+  const fetchPosts = async () => {
+    const response = await fetch("/api/prompt");
+    const data = await response.json();
+
+    setAllPosts(data);
   };
 
   useEffect(() => {
-    const fetchPosts = async () => {
-      const response = await fetch("/api/prompt");
-      const data = await response.json();
-
-      setPosts(data);
-    };
-
     fetchPosts();
   }, []);
 
+  const filterPrompts = (searchtext: string) => {
+    const regex = new RegExp(searchtext, "i"); // 'i' flag for case-insensitive search
+    return allPosts.filter(
+      (item: PromptType) =>
+        regex.test(item.creator.username) ||
+        regex.test(item.tag) ||
+        regex.test(item.prompt)
+    );
+  };
+
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (searchTimeout !== null){
+      clearTimeout(searchTimeout);
+    }
+    
+    setSearchText(e.target.value);
+
+    // debounce method
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = filterPrompts(e.target.value);
+        setSearchedResults(searchResult);
+      }, 500)
+    );
+  };
+
+  const handleTagClick = (tagName: string) => {
+    setSearchText(tagName);
+
+    const searchResult = filterPrompts(tagName);
+    setSearchedResults(searchResult);
+  };
+
   return (
-    <section className="feed">
-      <form className="relative w-full flex-center">
+    <section className='feed'>
+      <form className='relative w-full flex-center'>
         <input
-          type="text"
-          placeholder="Search for a tag or username"
+          type='text'
+          placeholder='Search for a tag or a username'
           value={searchText}
           onChange={handleSearchChange}
           required
-          className="search_input peer"
+          className='search_input peer'
         />
       </form>
 
-      <PromptCardList data={posts} handleTagClick={() => []} />
+      {/* All Prompts */}
+      {searchText ? (
+        <PromptCardList
+          data={searchedResults}
+          handleTagClick={handleTagClick}
+        />
+      ) : (
+        <PromptCardList data={allPosts} handleTagClick={handleTagClick} />
+      )}
     </section>
   );
 };
